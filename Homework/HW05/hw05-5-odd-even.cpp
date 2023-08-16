0d2bcd5c01ff9d52a5b302f9e37fc0518cf4c22b
RubyQWER <Chanatpong1234@gmail.com>
2023-08-17 01:40:14 +0700

    hw05-5

--- Homework/HW05/hw05-5-odd-even.cpp
+++ Homework/HW05/hw05-5-odd-even.cpp
@@ -16,3 +16,18 @@
         -22 is an even integer
 
 */
+#include <stdio.h>
+int main() {
+
+    int number ;
+
+    printf("Input : ") ;
+    scanf("%d",&number) ;
+
+    if (number %2 == 0) {
+        printf("%d is an even integer",number) ;
+    }
+        else printf("%d is an odd integer",number) ;
+    
+    return 0 ;
+}
